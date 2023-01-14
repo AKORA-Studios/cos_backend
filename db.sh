438d66e963da0a8104a1d8e7086501c8a2f0f204
AKORA <dragoncatm@gmail.com>
2023-01-14 21:34:43 +0100

    uwu

---
+++ db.sh
@@ -0,0 +1,7 @@
+docker run -d \
+    --name postgres-db \
+    -e POSTGRES_USER=cos \
+    -e POSTGRES_PASSWORD=SkQm8ujbSHs4U80gRoMl \
+    -p 5432:5432 \
+    -v cos_data:/var/lib/postgresql/data \
+    postgres:14-alpine3.16
