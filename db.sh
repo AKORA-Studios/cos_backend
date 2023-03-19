c7941bc4408c764facab5fe5945affa8b86577dc
AKORA <dragoncatm@gmail.com>
2023-03-19 15:28:24 +0100

    REmove db from docker-compose config

--- db.sh
+++
@@ -1,8 +0,0 @@
-docker run -d \
-    --name postgres-db \
-    --restart=always \
-    -e POSTGRES_USER=cos \
-    -e POSTGRES_PASSWORD=SkQm8ujbSHs4U80gRoMl \
-    -p 5432:5432 \
-    -v cos_data:/var/lib/postgresql/data \
-    postgres:14-alpine3.16
