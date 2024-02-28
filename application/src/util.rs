a72ba8c658bbe4590e5ef519773f50e8bdad1f8d
AKORA <dragoncatm@gmail.com>
2024-02-28 18:54:32 +0100

    Dont print out internal errors to users

--- application/src/util.rs
+++ application/src/util.rs
@@ -3,6 +3,9 @@ pub use shared::operation::*;
 pub fn map_sqlx_result<T>(result: Result<T, sqlx::Error>) -> Result<T, OpErr<String>> {
     result.map_err(|e| match e {
         sqlx::Error::RowNotFound => OpErr::NotFound("".to_owned()),
-        _ => OpErr::InternalError(e.to_string()),
+        _ => {
+            eprintln!("{e:?}");
+            OpErr::InternalError("Internal Server error :/".to_owned())
+        }
     })
 }
